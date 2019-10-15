ae950d3801e69c7b1c5404e62a6688de8d7e4ec3
CosmoDX <benjaminsalazarballadares@gmail.com>
2019-10-14 21:16:33 -0300

    Add an "Services" page and create the payload launcher.

---
+++ JS/main.js
@@ -0,0 +1,41 @@
+function detectOS()
+{
+    var os = "Not Detected...";
+    if (navigator.appVersion.indexOf("Win")!=-1) os="Windows";
+    if (navigator.appVersion.indexOf("Mac")!=-1) os="MacOS";
+    if (navigator.appVersion.indexOf("X11")!=-1) os="UNIX";
+    if (navigator.appVersion.indexOf("Linux")!=-1) os="Linux";
+
+    if(os == "Windows")
+    {
+        //alert("SORRY, You can't enter to this website  D:");
+    }
+
+    if(os == "MacOS")
+    {
+        alert("SORRY, HAS ESCOJIDO LA PUERTA EQUIBOCADA PERRA!");
+    }
+    
+    if(os == "Linux")
+    {
+        alert("If you are using android, you can't enter to this website!");
+    }
+
+    if(os == "UNIX")
+    {
+        alert("Nigga, WTF!?");
+    }
+}
+
+detectOS()
+
+function sayNotReady()
+{
+    alert("THIS ELEMENT IS NOT READY!")
+}
+
+function redirectPayloadLauncher()
+{
+    var redirectPayloadLauncher = "";
+    window.location.href = redirectPayloadLauncher;
+}
