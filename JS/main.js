b3eaca453764deac101ea1fb2c499b028b6be9ab
CosmoXDD <benjaminsalazarballadares@gmail.com>
2019-10-19 19:55:53 -0300

    Fix the Paint Game.

--- JS/main.js
+++ JS/main.js
@@ -36,19 +36,19 @@ function sayNotReady()
 
 function redirectPayloadLauncher()
 {
-    var redirectPayloadLauncher = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Functions/payloadLoader.html";
+    var redirectPayloadLauncher = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Services/payloadLoader.html";
     window.location.href = redirectPayloadLauncher;
 }
 
 function redirectGames()
 {
-    var redirectGamesPage = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Functions/Games/games.html"; 
+    var redirectGamesPage = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Services/Games/games.html"; 
     window.location.href = redirectGamesPage;
 }
 
 function redirectGameIndex()
 {
-    var redirectGameIndex = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Functions/Games/gameIndex.html"; 
+    var redirectGameIndex = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Services/Games/gameIndex.html"; 
     window.location.href = redirectGameIndex;
 }
 
@@ -56,6 +56,6 @@ function redirectGameIndex()
 
 function redirectToPaint()
 {
-    var gameLink = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Functions/Games/GameFiles/Paint/paint.html";
+    var gameLink = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Exploit/Services/Games/GameFiles/Paint/paint.html";
     window.location.href = gameLink;
 }
