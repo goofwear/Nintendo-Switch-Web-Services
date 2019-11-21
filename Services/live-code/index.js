f26eb282f1cb349db4702c7e914cdea584da24d2
CosmoDX <benjaminsalazarballadares@gmail.com>
2019-11-21 20:14:49 -0300

    Fixed the back buttons and added an HTML compiler for programers.

---
+++ Services/live-code/index.js
@@ -0,0 +1,5 @@
+function redirectToHtmlCompiler()
+{
+    var redirectLink = "https://cosmoxdd.github.io/Nintendo-Switch-Web-Services/Services/live-code/Compilers/HTML/Hcompiler.html";
+    window.location.href = redirectLink;
+}
